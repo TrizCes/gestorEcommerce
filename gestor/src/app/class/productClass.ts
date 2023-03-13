97d343ab8354a2f3e83259a6773ab59d236913fe
Beatriz Ceschini <biaceschini@gmail.com>
2023-03-13 14:35:26 -0300

    correção nos cadastros de produtos

--- gestor/src/app/class/productClass.ts
+++ gestor/src/app/class/productClass.ts
@@ -3,12 +3,13 @@ export class ProductClass {
   nome!: string;
   quantidade!: string;
   preco!: string;
+  criador?: string;
 
-
-  constructor(id: string, nome: string, quantidade: string, preco: string) {
+  constructor(id: string, nome: string, quantidade: string, preco: string, criador?: string) {
     this.id = id;
     this.nome = nome;
     this.quantidade = quantidade;
     this.preco = preco;
+    this.criador = criador;
 }
 }
--- gestor/src/app/components/product-record/product-record.component.ts
+++ gestor/src/app/components/product-record/product-record.component.ts
@@ -1,46 +1,56 @@
-import { Component, OnInit} from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 import { NameUserPipe } from 'src/app/pipes/name-user.pipe';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
 
 @Component({
   selector: 'app-product-record',
   templateUrl: './product-record.component.html',
   styleUrls: ['./product-record.component.scss'],
 })
-
-export class ProductRecordComponent implements OnInit{
+export class ProductRecordComponent implements OnInit {
   recordForm!: FormGroup;
   email!: any;
+  update!: any;
 
-  constructor(private fb: FormBuilder, private _httpClient: HttpClient){}
+  constructor(private fb: FormBuilder, private _httpClient: HttpClient) {}
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.email = localStorage.getItem('email');
     this.createNewProduct();
   }
 
   createNewProduct(): void {
     this.recordForm = new FormGroup({
-      id : new FormControl('', [Validators.required]),
+      id: new FormControl('', [Validators.required]),
       nome: new FormControl('', [Validators.required]),
       quantidade: new FormControl('', [Validators.required]),
       preco: new FormControl('', [Validators.required]),
-    })
-
+    });
   }
-  onSubmit(){
+  onSubmit() {
     if (!this.recordForm.valid) {
-      console.error("Erro ao cadastrar");
-      alert("Erro ao cadastrar");
+      console.error('Erro ao cadastrar');
+      alert('Erro ao cadastrar');
       return;
     }
-    alert(`Item cadastrado com sucesso`)
 
     const headers = new HttpHeaders().set('Content-Type', 'application/json');
-    this._httpClient.post('http://localhost:3000/list', this.recordForm.value , { headers }).subscribe(
-      (response: any) => console.log('Success:', response),
-      (error: any) => console.error('Error:', error));
+    this.update = this.recordForm.value;
+    this.update['criador'] = localStorage.getItem('email');
+    this._httpClient
+      .post('http://localhost:3000/list', this.recordForm.value, { headers })
+      .subscribe(
+        (response: any) => console.log('Success:', response),
+        (error: any) => console.error('Error:', error)
+      );
+
+    alert(`Item cadastrado com sucesso`);
+    this.recordForm.reset();
   }
 }
